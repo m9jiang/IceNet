0ecaaea4ba36e5acd8b7ac60b6cf7f249784c72a
mingzhe jiang <m9jiang@gmail.com>
2022-07-12 01:33:06 -0400

    Add incidence angle statistics

--- incidence_angle/split_near_far_range.py
+++ incidence_angle/split_near_far_range.py
@@ -10,10 +10,18 @@ def main(root: str, out: str, nfr: bool) -> None:
     dirs = [dir.split('_')[0] for dir in our_dirs]
     for scene in zip(dirs, our_dirs):
         # use beta_0 to sigma_0 ratio to get the incidence angle
-        sigma_cal_gains = minidom.parse(os.path.join(root, scene[0], scene[1] + '_HH', 'lutSigma.xml')).getElementsByTagName('gains')
-        gains = np.array([float(v) for v in sigma_cal_gains[0].firstChild.nodeValue.split(' ')])
-        beta_cal_gains = minidom.parse(os.path.join(root, scene[0], scene[1] + '_HH', 'lutBeta.xml')).getElementsByTagName('gains')
-        bgains = np.array([float(v) for v in beta_cal_gains[0].firstChild.nodeValue.split(' ')])
+        sigma_cal_gains = minidom.parse(
+            os.path.join(root, scene[0], scene[1] + '_HH', 'lutSigma.xml')
+        ).getElementsByTagName('gains')
+        gains = np.array(
+            [float(v) for v in sigma_cal_gains[0].firstChild.nodeValue.split(' ')]
+        )
+        beta_cal_gains = minidom.parse(
+            os.path.join(root, scene[0], scene[1] + '_HH', 'lutBeta.xml')
+        ).getElementsByTagName('gains')
+        bgains = np.array(
+            [float(v) for v in beta_cal_gains[0].firstChild.nodeValue.split(' ')]
+        )
         incidence_angle = np.arcsin(bgains/gains)*180/np.pi
         plt.plot(incidence_angle)
     print('Done')
